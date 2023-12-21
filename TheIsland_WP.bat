180c101aa0493483b3f4507d4ebccb5fb6451208
Naarin <30729156+Naarin@users.noreply.github.com>
2023-12-21 20:21:22 +0100

    Update TheIsland_WP.bat

--- TheIsland_WP.bat
+++ TheIsland_WP.bat
@@ -1,5 +1,6 @@
 @echo off
-
+echo #################### BACKUP ####################
+echo.
 set BackupName=%~n0_%Date:~-4%%Date:~3,2%%Date:~0,2%%Time:~0,2%%Time:~3,2%%Time:~6,2%
 xcopy  /e /q .\%~n0\ShooterGame\Saved\Config\WindowsServer\Game.ini .\Backup\%BackupName%\ShooterGame\Saved\Config\WindowsServer\
 xcopy  /e /q .\%~n0\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini .\Backup\%BackupName%\ShooterGame\Saved\Config\WindowsServer\
@@ -16,17 +17,29 @@ if not exist .\SteamCMD (
 	del .\steamcmd.zip
 	.\SteamCMD\steamcmd.exe +quit
 )
-
-mkdir .\%~n0\
+if not exist .\%~n0\ (
+	mkdir .\%~n0\
+)
+echo.
+echo #################### UPDATE ####################
+echo.
 .\SteamCMD\steamcmd.exe +force_install_dir ..\%~n0\ +login anonymous +app_update 2430930 +quit & :: update server
-rmdir /q /s .\%~n0\ShooterGame\Content\Movies & :: useless data
-
+if exist .\%~n0\ShooterGame\Content\Movies\ (
+	rmdir /q /s .\%~n0\ShooterGame\Content\Movies\
+)
+echo.
+echo #################### STARTE ####################
+@echo on
 start .\%~n0\ShooterGame\Binaries\Win64\ArkAscendedServer.exe %~n0 ?MultiHome=127.0.0.1 ?Port=7777 ?QueryPort=27015 ?SessionName=%~n0 ?ServerAdminPassword=1331 ?ServerPassword=1331 -NoBattlEye -noundermeshchecking -WinLiveMaxPlayers=1 & :: start server
+@echo off
 timeout 5
 powershell "$Process = Get-Process ArkAscendedServer ; $Process.ProcessorAffinity = 61440" & :: use 13/14 and 15/16 cores/threads (61440 = 0xF000 = 1111000000000000)
 
 :Backup
+echo.
+echo #################### BACKUP ####################
 timeout 1800
+echo.
 set BackupName=%~n0_%Date:~-4%%Date:~3,2%%Date:~0,2%%Time:~0,2%%Time:~3,2%%Time:~6,2%
 xcopy  /e /q .\%~n0\ShooterGame\Saved\Config\WindowsServer\Game.ini .\Backup\%BackupName%\ShooterGame\Saved\Config\WindowsServer\
 xcopy  /e /q .\%~n0\ShooterGame\Saved\Config\WindowsServer\GameUserSettings.ini .\Backup\%BackupName%\ShooterGame\Saved\Config\WindowsServer\
